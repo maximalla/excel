cb2d97f05806b4d6937b32a472e46041c4a88122
Maxim Shpontak <maximalla1997@gmail.com>
2023-08-17 22:47:36 +0300

    1708 new icon and deleted unused styles

--- src/app/services/input.service.ts
+++ src/app/services/input.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { AppConstants } from '../shared/constants/constants';
 import { Position } from '../shared/interfaces/position';
 import { ModelService } from '../shared/types/model.service';
 
@@ -13,21 +14,9 @@ export class InputService {
 
   getInputs(): void {
     window.addEventListener('keydown', (e) => {
-      if (
-        !this.m.isPaused &&
-        !this.m.gameOver &&
-        [
-          'ArrowUp',
-          'ArrowDown',
-          'ArrowLeft',
-          'ArrowRight',
-          'KeyW',
-          'KeyA',
-          'KeyS',
-          'KeyD',
-        ].includes(e.code)
-      ) {
-        this.setDirection(e.code);
+      if (!this.m.isPaused && !this.m.gameOver) {
+        const direction = AppConstants.keyToDirection[e.code];
+        if (direction) this.setDirection(direction);
       }
     });
   }
@@ -35,25 +24,21 @@ export class InputService {
   setDirection(direction: string): void {
     switch (direction) {
       case 'ArrowUp':
-      case 'KeyW':
         if (this.lastInputDirection.y !== 0) break;
         this.inputDirection = { x: 0, y: -1 };
         this.m.headTurn = 180;
         break;
       case 'ArrowDown':
-      case 'KeyS':
         if (this.lastInputDirection.y !== 0) break;
         this.inputDirection = { x: 0, y: 1 };
         this.m.headTurn = 0;
         break;
       case 'ArrowLeft':
-      case 'KeyA':
         if (this.lastInputDirection.x !== 0) break;
         this.inputDirection = { x: -1, y: 0 };
         this.m.headTurn = 90;
         break;
       case 'ArrowRight':
-      case 'KeyD':
         if (this.lastInputDirection.x !== 0) break;
         this.inputDirection = { x: 1, y: 0 };
         this.m.headTurn = -90;
--- src/app/shared/constants/constants.ts
+++ src/app/shared/constants/constants.ts
@@ -3,4 +3,19 @@ export class AppConstants {
 
   static readonly gridSizeX = 40;
   static readonly gridSizeY = 20;
+
+  static keyToDirection: { [key: string]: string } = {
+    ArrowUp: 'ArrowUp',
+    KeyW: 'ArrowUp',
+    Numpad8: 'ArrowUp',
+    ArrowDown: 'ArrowDown',
+    KeyS: 'ArrowDown',
+    Numpad2: 'ArrowDown',
+    ArrowLeft: 'ArrowLeft',
+    KeyA: 'ArrowLeft',
+    Numpad4: 'ArrowLeft',
+    ArrowRight: 'ArrowRight',
+    KeyD: 'ArrowRight',
+    Numpad6: 'ArrowRight',
+  };
 }
